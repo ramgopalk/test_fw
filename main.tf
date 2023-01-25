3db21604910b2d0bab475da08ca4696e1d3aa44e
ramgoplk <ramgopalkoya@gmail.com>
2023-01-25 15:48:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 
 provider "azurerm" {
   features {}
-  azurerm resource_group {
-     prevent_deletion_if_contains_resources = false
+  # azurerm resource_group {
+    #  prevent_deletion_if_contains_resources = false
 
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
-}
+# }
 
 # //provider
 # terraform {
