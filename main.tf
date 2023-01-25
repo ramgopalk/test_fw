952bbd6fdc9311e76b4bdabde644dab612c1954f
ramgoplk <ramgopalkoya@gmail.com>
2023-01-25 15:42:13 +0530

    up

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 
 provider "azurerm" {
   features {}
+  azurerm_resource_group {
+│       prevent_deletion_if_contains_resources = false
 
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
-
+}
 
 # //provider
 # terraform {
